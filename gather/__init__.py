42184805f31aba75d535a675b97a85c82179956c
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-09-02 10:58:45 +0000

    Put development code back into __init__ and fixed Procfile bug

--- gather/__init__.py
+++ gather/__init__.py
@@ -8,19 +8,17 @@ if os.path.exists("env.py"):
 
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
 
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
     uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
-
 
 db = SQLAlchemy(app)
 mongo = PyMongo(app)
