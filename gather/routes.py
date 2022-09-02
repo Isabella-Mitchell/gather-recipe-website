4e1e9b387f5360d289d208bbd8e5aac2aa5302da
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-09-02 14:45:54 +0000

    Set up registration functionality and flash messages

--- gather/routes.py
+++ gather/routes.py
@@ -14,4 +14,29 @@ def get_recipes():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # checks if username already exists
+        existing_user = User.query.filter(
+            User.user_name == request.form.get("user_name").lower()).all()
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+        
+        user = User(
+            user_name=request.form.get("user_name").lower(),
+            user_first_name=request.form.get("first_name").lower(),
+            user_last_name=request.form.get("last_name").lower(),
+            password=generate_password_hash(request.form.get("password"))
+        )
+        
+        db.session.add(user)
+        db.session.commit()
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("user_name").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("get_recipes", user_name=session["user"]))
+
     return render_template("register.html")
+
