cde3985a4a124835f6e8144a0759db02cea7c2ba
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-09-02 15:41:32 +0000

    Add login functionality

--- gather/routes.py
+++ gather/routes.py
@@ -40,3 +40,48 @@ def register():
 
     return render_template("register.html")
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = User.query.filter(User.user_name == 
+        request.form.get("user_name").lower()).all()
+
+        if existing_user:
+            print(request.form.get("user_name"))
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user[0].password, request.form.get("password")):
+                        session["user"] = request.form.get("user_name").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("user_name")))
+                        return redirect(url_for("get_recipes"))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+@app.route("/dashboard/<user_name>", methods=["GET", "POST"])
+def profile(user_name):
+        
+    if "user" in session:
+        return render_template("dashboard.html", user_name=session["user"])
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
