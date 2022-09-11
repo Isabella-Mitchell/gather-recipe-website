301bc52c1fc6345d47b73a74229898ff1e2a50ff
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-09-11 15:31:40 +0000

    Add view_recipe.html and create route

--- gather/routes.py
+++ gather/routes.py
@@ -161,6 +161,15 @@ def edit_recipe(recipe_id):
         "edit_recipe.html", recipe=recipe, cuisines=cuisines)
 
 
+@app.route("/view_recipe/<recipe_id>", methods=["GET"])
+def view_recipe(recipe_id):
+
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    cuisines = list(Cuisine.query.order_by(Cuisine.cuisine_name).all())
+    return render_template(
+        "view_recipe.html", recipe=recipe, cuisines=cuisines)
+
+
 @app.route("/delete_recipe/<recipe_id>")
 def delete_recipe(recipe_id):
 
