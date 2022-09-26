52dc9d9a0361ebc10da5162d9a0b4b45f4a4d9dc
Isabella-Mitchell <i.mitchell.0707@gmail.com>
2022-09-26 08:57:36 +0000

    Clean up JS code and add comments

--- gather/static/js/script.js
+++ gather/static/js/script.js
@@ -1,31 +1,9 @@
  //Functions
 
-
 /**
- * Sets the background colour of the recipe card image based on the recipe's colour code 
- * */
- function setRecipeImage() {
-    let recipeImages = document.getElementsByClassName("image-url");
-    let parent
-    let imageUrl
-    for(let i = 0; i < recipeImages.length; i++) {
-        parent = recipeImages[i].parentNode;
-        imageUrl = recipeImages[i].textContent;
-        if (imageUrl != ""){
-            parent.style.backgroundImage = "url(" + imageUrl + ")";
-            parent.style.backgroundRepeat = "no-repeat";
-            parent.style.backgroundSize = "cover";
-            parent.style.backgroundPosition = "center";
-        } else {
-            parent.classList.add("holding-image")
-        }
-    }
-};
-
-/**
- * Used in Edit Recipe form. Takes data stored as an array in MongoDB and presents it as a string.
- * Uses , to seperate list items
- * TO DO - Refactor array/ string functions. Or change to use JSON
+ * Used in Edit Recipe form for ingrediants and tags. 
+ * Takes data stored as an array in MongoDB and presents it as a string to the user.
+ * Uses commas (,) to seperate items
  * */
 function turnArrayIntoString() {
     let arrayContainers = document.getElementsByClassName("array-string");
@@ -41,9 +19,9 @@ function turnArrayIntoString() {
 };
 
 /**
- * Used in Edit Recipe form. Takes instructions data stored as an array in MongoDB and presents it as a string.
- * Uses \r\n to seperate steps
- * TO DO - Refactor array/ string functions. Or change to use JSON
+ * Used in Edit Recipe form for instructions
+ * Takes data stored as an array in MongoDB and presents it as a string to the user.
+ * Uses new line (\r\n) to seperate items
  * */
 function turnStepsArrayIntoString() {
     let arrayContainers = document.getElementsByClassName("array-steps-string");
@@ -68,18 +46,13 @@ function turnStepsArrayIntoString() {
  * returns the list
  * */
 function makeUL(array) {
-    // Create the list element:
     let list = document.createElement('ul');
     for (let i = 0; i < array.length; i++) {
-        // Create the list item:
         let item = document.createElement('li');
-        // Set its contents:
         item.appendChild(document.createTextNode(array[i]));
-        // Add it to the list:
         list.appendChild(item);
     }
 
-    // Finally, return the constructed list:
     return list;
 }
 
@@ -88,27 +61,22 @@ function makeUL(array) {
  * returns the list
  * */
 function makeOL(array) {
-    // Create the list element:
     let list = document.createElement('ol');
     for (let i = 0; i < array.length; i++) {
-        // Create the list item:
         let item = document.createElement('li');
-        // Set its contents:
         item.appendChild(document.createTextNode(array[i]));
-        // Add it to the list:
         list.appendChild(item);
     }
-    // Finally, return the constructed list:
+
     return list;
 }
 
 
 /**
- * Used on Find Recipes, Dashboard and View Recipes. 
+ * Used on Recipe Cards on various pages on website
+ * Used for ingrediant list and tags 
  * Takes data stored as an array in MongoDB, removes formatting and turns it into an JS array.
- * Uses , to seperate list items
- * Calls makeUL function to show array as an unordered list
- * TO DO - Refactor array/ string functions. Or change to use JSON
+ * Calls the makeUL function to show array as an unordered list on the recipe cards
  * */
 function turnStringIntoArray() {
     let stringContainers = document.getElementsByClassName("string-to-array")
@@ -121,18 +89,16 @@ function turnStringIntoArray() {
         removeBrackets = string.replace(/\[|\]/g, '');
         removeQuotes = removeBrackets.replace(/['"]+/g, '')
         array = removeQuotes.split(", ");
-        // Add the contents
         stringContainers[i].textContent = ""
         stringContainers[i].appendChild(makeUL(array));
     }
 };
 
 /**
- * Used on Find Recipes, Dashboard and View Recipes. 
+ * Used on Recipe Cards on various pages on website
+ * Used for instructions
  * Takes data stored as an array in MongoDB, removes formatting and turns it into an JS array.
- * Uses ', ' to seperate steps
  * Calls makeOIL function to show array as an ordered list
- * TO DO - Refactor array/ string functions. Or change to use JSON
  * */
 function turnStepsStringIntoArray() {
     let stringContainers = document.getElementsByClassName("steps-string-to-array")
@@ -153,11 +119,14 @@ function turnStepsStringIntoArray() {
 };
 
 $(document).ready(function(){
+    // initiates mobile slide out nav bar from materialise css
     $('.sidenav').sidenav();
+    // initiates select input for forms from materialise css
     $('select').formSelect();
+    // initiates tooltips used on buttons
     $('.tooltipped').tooltip();
-    $('.modal').modal();
 
+    // validates materialize select field so user must select a cuisine
     validateMaterializeSelect();
     function validateMaterializeSelect() {
         let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
@@ -192,7 +161,7 @@ $(document).ready(function(){
     turnStepsStringIntoArray();
     setRecipeImage();
 
-    //To stop people entering instead of using commas.
+    //To stop people entering a new line instead of using commas on ingrediants list.
     $('#ingrediant_list').keypress(function (e) {
         if (e.which == 13) {
             e.preventDefault();
